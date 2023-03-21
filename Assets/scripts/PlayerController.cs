04fec71af141f7a458bb0473d1e6d675e641c03a
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-03-20 23:39:18 -0700

    experimenting with backgrounds (scared to commit)

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,20 +7,26 @@ public class PlayerController : MonoBehaviour
 
     public Camera sceneCamera;
     public float moveSpeed;
+    
+    // private float minSpeed = 1f;
     public Rigidbody2D rb;
     public Weapon weapon;
     private Vector2 moveDirection;
     private Vector2 mousePosition;
 
     // Update is called once per frame
+    // use for things that need to happen instantly, regardless of other things happening in game.
     void Update()
     {
         ProcessInputs();
     }
 
+    // FixedUpdate is called every 0.2 seconds
+    // use for things that need to occur in a controlled amount of time (like physics)
     void FixedUpdate()
     {
         Move();
+
     }
 
     void ProcessInputs()
@@ -38,8 +44,8 @@ public class PlayerController : MonoBehaviour
 
     void Move()
     {
+       
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-
 
         // rotate player to follow the mouse
         Vector2 aimDirection = mousePosition - rb.position;
--- Assets/scripts/RoomSpawner.cs
+++ Assets/scripts/RoomSpawner.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
+    public int openingDirection;
+    private int rand;
+    public bool spawned = false;
+    // 1 ==> need bottom door.
+    // 2 ==> neet top door.
+    // 3 ==> need left door.
+    // 4 ==> right door
+
+    private RoomTemplates templates;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        templates = GameObject.FindGameObjectWithTag("rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
     }
 
     // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        
+        if (spawned == false)
+        {
+
+            if(openingDirection == 1)
+            {
+                rand = Random.Range(0, templates.bottomRooms.Length);
+                Instantiate(templates.bottomRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if(openingDirection ==2) 
+            {
+                rand = Random.Range(0, templates.topRooms.Length);
+                Instantiate(templates.topRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if(openingDirection ==3) 
+            {
+                rand = Random.Range(0, templates.leftRooms.Length);
+                Instantiate(templates.leftRooms[rand], transform.position, Quaternion.identity);
+            }
+            else if(openingDirection ==4) 
+            {
+                rand = Random.Range(0, templates.rightRooms.Length);
+                Instantiate(templates.rightRooms[rand], transform.position, Quaternion.identity);
+            }
+            spawned = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("spawner") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
     }
 }
